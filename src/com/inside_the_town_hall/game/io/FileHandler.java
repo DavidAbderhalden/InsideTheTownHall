a20e4a4bcc746afb6f758254e7bcec38b4e1b41a
David-Abderhalden-1 <david.abderhalden@hotmail.com>
2023-01-19 10:59:11 +0100

    Implemented basic file io

---
+++ src/com/inside_the_town_hall/game/io/FileHandler.java
@@ -0,0 +1,66 @@
+package com.inside_the_town_hall.game.io;
+
+import com.inside_the_town_hall.game.translation.LanguageManager;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Handles all the file input and output streams.
+ * Is a Singleton
+ *
+ * @author NekroQuest
+ */
+public class FileHandler {
+    private final String CONTENT_ROOT; // The root directory
+    private static FileHandler instance;
+
+    public static FileHandler getInstance() {
+        if(instance == null) {
+            instance = new FileHandler(""); // TODO: Maybe outsource to config
+        } return instance;
+    }
+
+    private FileHandler(String CONTENT_ROOT) {
+        this.CONTENT_ROOT = CONTENT_ROOT;
+    }
+
+    /**
+     * @param path the relative path to the file
+     * @return byte array content of the file
+     */
+    public static byte[] readFileAsBytes(String path) {
+        return String.valueOf(fileInputStream(path)).getBytes();
+    }
+
+    /**
+     * @param path the relative path to the file
+     * @return string content of the file
+     */
+    public static String readFile(String path) {
+        return fileInputStream(path).toString();
+    }
+
+    /**
+     * Reads a file content and handles exceptions
+     *
+     * @param path the relative path to the file
+     * @return string Buffer with the file content
+     */
+    private static StringBuilder fileInputStream(String path) {
+        BufferedInputStream bufferedInputStream;
+        StringBuilder fileContent = new StringBuilder();
+        try {
+            bufferedInputStream = new BufferedInputStream(
+                    new FileInputStream(path)
+            );
+            while(bufferedInputStream.available() > 0) {
+                fileContent.append((char)bufferedInputStream.read());
+            }
+        } catch (IOException e) {
+            LanguageManager.getInstance().use("ERROR.IO.FILE.INPUT");
+        }
+        return fileContent;
+    }
+}
